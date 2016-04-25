028de22962c90cfe2a4967079143730374378aa6
Casper <casper.van.beuzekom@boostermedia.com>
2016-04-25 15:04:02 +0200

    started working on waiter

---
+++ src/async/Waiter.js
@@ -0,0 +1,14 @@
+class Waiter
+{
+    constructor()
+    {
+        console.log('Waiter.ctor');
+    }
+    
+    waitFor()
+    {
+        console.log('Waiter.waitFor');
+    }
+}
+
+export default Waiter;
