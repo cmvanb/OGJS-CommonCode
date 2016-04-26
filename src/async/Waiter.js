143255ba79681696e638603f332bcd9bc05bd107
Casper <casper.van.beuzekom@boostermedia.com>
2016-04-26 10:24:27 +0200

    switched to 80 char line limit

--- src/async/Waiter.js
+++ src/async/Waiter.js
@@ -1,6 +1,6 @@
 /**
- * A waiter object waits for an arbitrary number of asynchronous processes. Each async process is
- * encapsulated in a callback-context-argument object.
+ * A waiter object waits for an arbitrary number of asynchronous processes. 
+ * Each async process is encapsulated in a callback-context-argument object.
  *
  * @author Casper
  */
@@ -22,34 +22,42 @@ class Waiter
     }
     
     /**
-     * Adds a callback-context-arguments object to an array, to be executed asynchronously. When all
-     * 'waitFor' and 'andFor' callbacks have been fired, it will call the final 'then' callback.
-     *
+     * Adds a callback-context-arguments object to an array, to be executed 
+     * asynchronously. When all 'waitFor' and 'andFor' callbacks have been 
+     * fired, it will call the final 'then' callback.
+     * 
      * @param callback
      * @param context
      * @returns {Object}
      */
     waitFor(callback, context)
     {
-        console.assert(!this._inProgress, "Waiter.waitFor: " + Waiter.ERRORS.ALREADY_IN_PROGRESS);
+        console.assert(
+            !this._inProgress,
+            "Waiter.waitFor: " + Waiter.ERRORS.ALREADY_IN_PROGRESS);
         
-        // This function is basically syntactic sugar, so just re-use the perfectly fine andFor().
+        // This function is basically syntactic sugar, so just reuse the
+        // perfectly fine andFor().
         return this.andFor.apply(this, arguments);
     }
     
     /**
-     * Adds a callback-context-arguments object to an array, to be executed asynchronously. When all
-     * 'waitFor' and 'andFor' callbacks have been fired, it will call the final 'then' callback.
-     *
+     * Adds a callback-context-arguments object to an array, to be executed 
+     * asynchronously. When all 'waitFor' and 'andFor' callbacks have been 
+     * fired, it will call the final 'then' callback.
+     * 
      * @param callback
      * @param context
      * @returns {Object}
      */
     andFor(callback, context)
     {
-        console.assert(!this._inProgress, "Waiter.andFor: " + Waiter.ERRORS.ALREADY_IN_PROGRESS);
+        console.assert(
+            !this._inProgress,
+            "Waiter.andFor: " + Waiter.ERRORS.ALREADY_IN_PROGRESS);
         
-        var nextObject = CallbackUtil.createCallbackContextObject.apply(this, arguments);
+        let nextObject = CallbackUtil.createCallbackContextObject.apply(
+            this, arguments);
 
         this._callbackContextObjects.push(nextObject);
 
@@ -57,21 +65,24 @@ class Waiter
     }
 
     /**
-     * Stores the final 'then' callback, listens for the completion of all previous callbacks, and
-     * when successfull, triggers the final callback.
+     * Stores the final 'then' callback, listens for the completion of all 
+     * previous callbacks, and when successfull, triggers the final callback.
      *
      * @param callback
      * @param context
      */
     then(callback, context)
     {
-        console.assert(!this._inProgress, "Waiter.then: " + Waiter.ERRORS.ALREADY_IN_PROGRESS);
+        console.assert(
+            !this._inProgress,
+            "Waiter.then: " + Waiter.ERRORS.ALREADY_IN_PROGRESS);
         
-        var finalObject =  CallbackUtil.createCallbackContextObject.apply(this, arguments);
+        let finalObject = CallbackUtil.createCallbackContextObject.apply(
+            this, arguments);
 
-        var callbacksCount = this._callbackContextObjects.length;
+        let callbacksCount = this._callbackContextObjects.length;
 
-        var callbacksCompleted = 0;
+        let callbacksCompleted = 0;
 
         function onCallbackCompleted()
         {
@@ -79,7 +90,8 @@ class Waiter
 
             if (callbacksCompleted === callbacksCount)
             {
-                finalObject.callback.apply(finalObject.context, finalObject.args);
+                finalObject.callback.apply(
+                    finalObject.context, finalObject.args);
             }
         }
 
@@ -87,12 +99,12 @@ class Waiter
 
         this._callbackContextObjects.forEach(function(callbackContextObject)
         {
-            var args = [ onCallbackCompleted, this ];
+            let args = [ onCallbackCompleted, this ];
 
-            // Concatenate any extra arguments the pair should receive.
             Array.prototype.push.apply(args, callbackContextObject.args);
 
-            callbackContextObject.callback.apply(callbackContextObject.context, args);
+            callbackContextObject.callback.apply(
+                callbackContextObject.context, args);
         }, this);
     }
 }
--- src/utils/CallbackUtil.js
+++ src/utils/CallbackUtil.js
@@ -1,8 +1,8 @@
 class CallbackUtil
 {
     /**
-     * Create a js object encapsulating a callback function and it's context, and any additional
-     * arguments.
+     * Create a js object encapsulating a callback function and it's context, 
+     * and any additional arguments.
      *
      * @param callback
      * @param context
@@ -10,9 +10,10 @@ class CallbackUtil
      */
     static createCallbackContextObject(callback, context)
     {
-        var args = Array.prototype.slice.call(arguments);
+        let args = Array.prototype.slice.call(arguments);
 
-        // Splice out callback and context, we want to pass any extra arguments as an array.
+        // Splice out callback and context, we want to pass any extra arguments
+        // as an array.
         args.splice(0, 2);
 
         return {
