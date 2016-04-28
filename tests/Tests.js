e0462afad2d03841f950db60d59023ddeea80642
Casper <casper.van.beuzekom@boostermedia.com>
2016-04-28 13:47:03 +0200

    module loading wip

---
+++ tests/Tests.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//System.import("../lib/qunit/qunit.js");
+//System.import("./utils/TestCallbackUtil.js");
+
+import * as QUnit from "../lib/qunit/qunit";
+import * as TestCallbackUtil from "utils/TestCallbackUtil";
+
+(function()
+{
+    console.log("WTF");
+    
+    QUnit.load();
+    QUnit.start();
+}());
---
+++ tests/utils/TestCallbackUtil.js
@@ -0,0 +1,30 @@
+"use strict";
+
+//System.import("../lib/qunit/qunit");
+//System.import("./utils/TestCallbackUtil");
+
+import * as QUnit from "../../lib/qunit/qunit";
+import * as CallbackUtil from "../../src/utils/CallbackUtil";
+
+QUnit.test('CallbackUtil.createCallbackContextObject', function(assert)
+{
+    let callback = function(){};
+
+    let context = this;
+
+    let argument1 = 1;
+
+    let argument2 = 1;
+    
+    let argument3 = 1;
+    
+    let callbackContextObject = CallbackUtil.createCallbackContextObject(
+        callback,
+        context,
+        argument1,
+        argument2,
+        argument3);
+
+    assert.propEqual(true, false,
+        'Constructor correctly intializes all the values of the component.');
+});
